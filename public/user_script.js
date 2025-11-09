0ebe7c2fa58d413fce49170dd304a825130f21c4
rulo-codes <150222307+rulo-codes@users.noreply.github.com>
2025-11-09 21:30:02 +0800

    added post and load post feature on user page

--- public/user_script.js
+++ public/user_script.js
@@ -7,5 +7,35 @@ document.getElementById('logoutBtn').addEventListener('click', async () => {
     }
 });
 
+document.getElementById('diary_post').addEventListener('click', async () => {
+    const diaryInput = document.getElementById('diary_input').value;
+
+    if(!diaryInput.trim()) return alert("Please write something");
+
+    const res = await fetch('/api/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({diaryInput})
+    });
+
+    const data = await res.json();
+    if(data.success){
+        alert('Diary Posted!');
+        document.getElementById('diary_input') = "";
+    } else {
+        alert('Failed to post.')
+    }
+});
+
+async function loadDiaries(){
+    const res = await fetch('/api/posts');
+    const diaries = await res.json();
+
+    const container = documents.getElementById('diaries');
+    container.innerHtml = diaries.map(i = `<p>${i.content}</p>`).join('');
+};
+
 const user = localStorage.getItem('username');
 if (user) document.getElementById('userName').textContent = `${user}`;
